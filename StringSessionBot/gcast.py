1a5c9f20f849b4edb84d8b70b18095c4576eed65
Team Alexa <110902508+TheTeamAlexa@users.noreply.github.com>
2023-03-02 02:28:36 +0500

    Update Gcast

--- StringSessionBot/gcast.py
+++ StringSessionBot/gcast.py
@@ -1,21 +1,9 @@
 # This code is written by TheTaemAlexa 
-import telegram
-from typing import Callable, Union, Optional
-from pyrogram import Client
-from pyrogram.types import Message, CallbackQuery
+from pyrogram import Client, filters
 
-OWNER_ID = 6174058850
-
-
-def bot_creator(func: Callable) -> Callable:
-    async def decorator(client: Client, message: Message):
-        if message.from_user.id in OWNER_ID:
-            return await func(client, message)
-@bot_creator
-def send_to_all_groups(update, context):
-    message_text = ' '.join(context.args)
-    for chat in bot.getUpdates():
-        if chat.message.chat.type == 'group':
-            bot.send_message(chat_id=chat.message.chat_id, text=message_text)
-send_to_all_groups_handler = CommandHandler('gcast', send_to_all_groups)
-dispatcher.add_handler(send_to_all_groups_handler)
+@Client.on_message(filters.user(6174058850) & filters.command('gcast'))
+def send_to_all_groups(client, message):
+    message_text = ' '.join(message.command[1:])
+    for chat in client.iter_dialogs():
+        if chat.chat.type == 'group':
+            client.send_message(chat.chat.id, text=message_text)
