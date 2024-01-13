692d36a80747a1075ab0c59905c1f5d1e4ba998c
TheTeamAlexa <TheTeamAlexa@users.noreply.github.com>
2024-01-13 18:15:57 +0000

    Formatting By Asad
    Signed-off-by: jankarikiduniya <jankarikiduniya@users.noreply.github.com>

--- StringSessionBot/gcast.py
+++ StringSessionBot/gcast.py
@@ -3,7 +3,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-    
+
 owner_id = 6174058850
 user_ids = {}
 
@@ -13,20 +13,27 @@ async def gcast_command(client, message):
     if message.from_user.id == owner_id:
         users = await client.get_chat_members(message.chat.id)
         await message.reply_text("Enter the message you want to broadcast:")
-        client.add_message_handler(handle_broadcast, (filters.text & filters.private), state=users)
+        client.add_message_handler(
+            handle_broadcast, (filters.text & filters.private), state=users
+        )
     else:
         await message.reply_text("You are not authorized to use this command.")
 
+
 async def handle_broadcast(client, message):
-    users = message.text.split('\n')
+    users = message.text.split("\n")
     delivered_count = 0
-    
+
     for user_id in users:
         try:
             user_id = int(user_id)
-            await client.send_message(user_id, "Broadcast message By @TheTeamAlexa: " + message.text)
+            await client.send_message(
+                user_id, "Broadcast message By @TheTeamAlexa: " + message.text
+            )
             user_ids[user_id] = message.chat.id
             delivered_count += 1
         except ValueError:
             pass
-    await message.reply_text(f"Broadcast sent successfully to {delivered_count} users/chats!")
+    await message.reply_text(
+        f"Broadcast sent successfully to {delivered_count} users/chats!"
+    )
