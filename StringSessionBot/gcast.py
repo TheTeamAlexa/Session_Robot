15488ab8c0864966ffa8be6ea96d89491db7b8bf
Team Alexa <110902508+TheTeamAlexa@users.noreply.github.com>
2024-01-14 00:02:03 +0500

    Added pickledb

--- StringSessionBot/gcast.py
+++ StringSessionBot/gcast.py
@@ -1,39 +1,52 @@
 # This code is written by TheTaemAlexa
 
+import pickledb
+from Data import Data
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import InlineKeyboardMarkup, Message
 
+# Initialize the database
+db = pickledb.load('user_db.db', False)
 
+# Filters to use command
+def filter(cmd: str):
+    return filters.private & filters.incoming & filters.command(cmd)
+
+# Owner id
 owner_id = 6174058850
-user_ids = {}
 
+# Start Message
+@Client.on_message(filter("start"))
+async def start(bot: Client, msg: Message):  # Corrected 'message' to 'msg'
+    user = await bot.get_me()
+    user_id = msg.from_user.id  # Corrected 'message' to 'msg'
+    if not db.get(str(user_id)):
+        db.set(str(user_id), True)
+    mention = user.mention
+    await bot.send_message(
+        msg.chat.id,
+        Data.START.format(msg.from_user.mention, mention),
+        reply_markup=InlineKeyboardMarkup(Data.buttons),
+    )
 
 @Client.on_message(filters.command("gcast") & filters.private)
 async def gcast_command(client, message):
     if message.from_user.id == owner_id:
-        users = await client.get_chat_members(message.chat.id)
         await message.reply_text("Enter the message you want to broadcast:")
-        client.add_message_handler(
-            handle_broadcast, (filters.text & filters.private), state=users
-        )
+        client.add_message_handler(handle_broadcast)
     else:
         await message.reply_text("You are not authorized to use this command.")
 
-
 async def handle_broadcast(client, message):
-    users = message.text.split("\n")
+    users = db.getall()
     delivered_count = 0
 
     for user_id in users:
         try:
             user_id = int(user_id)
-            await client.send_message(
-                user_id, "Broadcast message By @TheTeamAlexa: " + message.text
-            )
-            user_ids[user_id] = message.chat.id
+            await client.send_message(user_id, "Broadcast message By @TheTeamAlexa: " + message.text)
             delivered_count += 1
         except ValueError:
             pass
-    await message.reply_text(
-        f"Broadcast sent successfully to {delivered_count} users/chats!"
-    )
+
+    await message.reply_text(f"Broadcast sent successfully to {delivered_count} users!")
